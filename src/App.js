e517b97473414e87802033eeb85e5da54cffc130
Master <ahnb21@gmail.com>
2020-05-27 14:21:02 +0900

    # practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = { //object  
+    count: 0
+  };
+  add = () => { //setState를 호출할 때마다 새로운 state와 함께 render를 다시한다.
+    this.setState(current => ({ count: current.count + 1})); //current는 현재 값을 가져온다.
+  };
+  minus = () => {
+    this.setState(current => ({count: current.count - 1}));
+  };
+  render() { //리액트는 자동으로 class component의 render method를 실행한다.
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>ADD</button>
+        <button onClick={this.minus}>MINUS</button>
+      </div>
+    )
+  }
 }
 
 export default App;

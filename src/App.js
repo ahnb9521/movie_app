eace7f405ec86463a74a8766a33cbfd56c37e482
Master <ahnb21@gmail.com>
2020-05-27 17:38:43 +0900

    # 디자인까지 완료!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
-class App extends React.Component{
-  state = { //object  
-    count: 0
-  };
-  add = () => { //setState를 호출할 때마다 새로운 state와 함께 render를 다시한다.
-    this.setState(current => ({ count: current.count + 1})); //current는 현재 값을 가져온다.
-  };
-  minus = () => {
-    this.setState(current => ({count: current.count - 1}));
-  };
-  render() { //리액트는 자동으로 class component의 render method를 실행한다.
-    return (
-      <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>ADD</button>
-        <button onClick={this.minus}>MINUS</button>
-      </div>
-    )
-  }
+class App extends React.Component {
+    state = { //object
+        isLoading: true,
+        movies: []
+    };
+
+    getMovies = async () => {
+        const {
+            data: {
+                data: {
+                    movies
+                }
+            }
+        } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+        this.setState({movies: movies, isLoading: false})
+    }
+
+    componentDidMount() { //render할 때 제일 먼저 호출 (Life Cycle)
+        this.getMovies();
+    };
+
+    render() { //리액트는 자동으로 class component의 render method를 실행한다.
+        const {isLoading, movies} = this.state;
+        return (
+            <section className="container">{
+                    isLoading
+                        ? <div className="loader">
+                              <span className="loader__text">Loading...</span>
+                          </div>
+                        : <div className="movie__div">
+                             {movies.map(movie => (
+                               <Movie
+                                  key={movie.id}
+                                  id={movie.id}
+                                  year={movie.year}
+                                  title={movie.title}
+                                  summary={movie.summary}
+                                  poster={movie.medium_cover_image}
+                                  genres={movie.genres}
+                               />
+                             ))}
+                          </div>
+                        
+                }</section>
+        );
+    }
 }
 
 export default App;

152890d1aeedd56f8e4810a44e911d340fdfd2d6
Master <ahnb21@gmail.com>
2020-05-28 09:09:44 +0900

    Updates

---
+++ precache-manifest.5b16e5183e1cad1ee158f3f1402cf2dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c391fae2786aa4c7d5a1e32b0b1b3d8",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "959570d586bfe30a1fe0",
+    "url": "/movie_app/static/css/main.fe982c34.chunk.css"
+  },
+  {
+    "revision": "78a8c6d4641937053d35",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "959570d586bfe30a1fe0",
+    "url": "/movie_app/static/js/main.1518a6e2.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
